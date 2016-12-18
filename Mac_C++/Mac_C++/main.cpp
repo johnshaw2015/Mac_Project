2c324193571f4d71a121ac88d37c25422cd5a018
johnshaw2015 <johnshaw0913@gmail.com>
2016-12-18 22:14:47 +0800

    中英文混合字符串截取

--- Mac_C++/Mac_C++/main.cpp
+++ Mac_C++/Mac_C++/main.cpp
@@ -7,9 +7,79 @@
 //
 
 #include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define CHINESE_CHAR_LENGTH_UTF8 3  // 根据编码格式定义一个中文字符占几个字节(默认为UTF-8,三个字节)
+
+using namespace std;
+
+/*
+ brief 截取字符串
+ param start 起始下标，从1开始
+ param end   结束下标
+ param isNeedPoint 是否需要在末尾添加“...”
+ return 截取之后的字符串
+ */
+string subNickName(string str,int start, int end, bool isNeedPoint)
+{
+    vector<string> result;   // 存储解析之后的字符串
+    int i = 0;
+    while (i < str.length()){
+        if (~(str.at(i) >> 8) == 0){
+            //代表不是汉字
+            result.push_back(str.substr(i, CHINESE_CHAR_LENGTH_UTF8));  // 一个汉字三个字节
+            i = i + CHINESE_CHAR_LENGTH_UTF8;
+        }else{
+            result.push_back(str.substr(i, 1));  // 一个英文一个字节
+            i = i + 1;
+        }
+    }
+    
+    // 容错处理，如果end大于字符串长度，则舍弃多余部分
+    end = (result.size()) >= end ? end : (result.size());
+    
+    string temp = "";
+    //直接从_result里取即可
+    for (int i = start; i <= end; i++){
+        temp += result[i - 1];
+    }
+    
+    // 如果字符串太长，在末尾添加“...”
+    if (isNeedPoint){
+        if ((result.size()) > end){
+            temp += "..";
+        }
+    }
+    
+    return temp;
+}
+
+int main(int argc, const char* argv[]) {
+    //排序
+    double result[4] = {63,345,34,897};
+    //自动排序：升序
+    map<double,int> mapResult;
+    mapResult.clear();
+    for (int i = 0; i < 4; i++) {
+        mapResult.insert(pair<double, int>(result[i],i));
+    }
+    
+    map<double,int>::iterator iter;
+    for (iter = mapResult.begin(); iter != mapResult.end(); iter++) {
+        printf("%f = %d\n",iter->first,iter->second);
+    }
+    
+    iter = mapResult.end(); //end()指向最后一个元素的后面
+    iter--;
+    int iMaxValue = iter->second;
+    printf("iMaxValue = %d\n", iMaxValue);
+    
+    string str  ="最好放不是你法案是sadfasgfaqw";
+    
+    string temp = subNickName(str,1,4,true);
+    cout << temp << endl;
+    
     return 0;
 }
