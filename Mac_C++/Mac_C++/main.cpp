9b3a351ad8dd9f57f9cacac6c6918e95f49856f8
johnshaw2015 <johnshaw0913@gmail.com>
2016-12-18 22:07:22 +0800

    c++ learn project

---
+++ Mac_C++/Mac_C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mac_C++
+//
+//  Created by John on 2016/12/18.
+//  Copyright © 2016年 John. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
